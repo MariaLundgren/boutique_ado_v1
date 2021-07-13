a1692963a58ba7a25407ca02efc8b5bd2c09c71f
MariaLundgren <melundgren93@gmail.com>
2021-07-13 19:37:43 +0000

    Updated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -5,23 +5,23 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
-    delivery information and oredr history
+    delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label="Country *", null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
 
-
-def __str__(self):
-    return self.user.username
+    def __str__(self):
+        return self.user.username
 
 
 @receiver(post_save, sender=User)
@@ -31,5 +31,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Excisting users: just save the profile
+    # Existing users: just save the profile
     instance.userprofile.save()
