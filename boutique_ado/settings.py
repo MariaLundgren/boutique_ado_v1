4c773bee19da388045ebda42635f556cb0d87a42
MariaLundgren <melundgren93@gmail.com>
2021-07-18 17:01:48 +0000

    added env.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+if os.path.exists("env.py"):
+    import env 
+
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -19,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
